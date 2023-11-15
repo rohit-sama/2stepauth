275787d8c511f81c8779cf805e3820f5587cd597
rohit-sama <rohit9804singh@gmail.com>
2023-11-15 15:56:11 +0530

    docs page

---
+++ frontend/2stepauth/src/app/docs/page.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/header'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <Header />
+        <h1 className='text-gray-200 lg:text-4xl md:text-3xl text-2xl m-10 flex justify-center items center'>Documentation</h1>
+        
+        </div>
+  )
+}
+
+export default page
