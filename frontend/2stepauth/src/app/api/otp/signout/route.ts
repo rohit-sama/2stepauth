3d43901aa20c87a3bc684df993a4d5fd26cc72f8
rohit-sama <rohit9804singh@gmail.com>
2023-11-15 13:48:23 +0530

    signout function

---
+++ frontend/2stepauth/src/app/api/otp/signout/route.ts
@@ -0,0 +1,41 @@
+import { db } from "@/lib/db";
+
+export async function POST(req: Request, res: Response) {
+    try {
+        const { apikey, email } = await req.json();
+        const key = `user:${apikey}:dev`;
+        const devExists = await db.sismember(key, apikey);
+
+        if (!devExists) {
+            const errorResponse = {
+                error: true,
+                message: "Invalid apikey",
+            };
+            return new Response(JSON.stringify(errorResponse), { status: 401, headers: { 'Content-Type': 'application/json' } });
+        }
+
+        const successResponse = {
+            message: "session True",
+        };
+
+        const key1 = `user:${email}:session`;
+        const sessionExists = await db.sismember(key1, true);
+        if (sessionExists) {
+            // Remove the existing session key
+            await db.srem(key1, true);
+
+            // Set the session to false
+            await db.sadd(key1, false);
+            return new Response(JSON.stringify(successResponse), { status: 200, headers: { 'Content-Type': 'application/json' } });
+        } else {
+            const errorResponse = {
+                error: true,
+                message: "Session False",
+            };
+            return new Response(JSON.stringify(errorResponse), { status: 401, headers: { 'Content-Type': 'application/json' } });
+        }
+
+    } catch (error) {
+        return new Response(JSON.stringify(error), { status: 500, headers: { 'Content-Type': 'application/json' } });
+    }
+}
